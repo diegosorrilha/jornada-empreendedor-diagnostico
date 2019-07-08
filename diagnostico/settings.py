4509b56402c872bf6ca0f73a4bd54a232c8fa660
Diego Sorrilha <diego@sorrilha.net>
2019-07-08 14:40:06 -0300

    Extraida configuração de banco de dados
    close #12

--- diagnostico/settings.py
+++ diagnostico/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from functools import partial
 
+import dj_database_url
 from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -76,11 +78,11 @@ WSGI_APPLICATION = 'diagnostico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+default_db_url = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
 
 
