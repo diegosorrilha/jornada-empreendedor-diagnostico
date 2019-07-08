283dd1118ea225989b8498a873d738ac590dfcfb
Diego Sorrilha <diego@sorrilha.net>
2019-07-08 13:34:23 -0300

    Instalada python-decouple e extraida configuração DEBUG
    close #9

--- diagnostico/settings.py
+++ diagnostico/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kou@g&s5$&mt_(rpbs4e3#x3ea$y3$3n&^&e9+r6a=sv!g(73='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
