9259c6f2b54d87ccd3ec10514af873648fafdaa8
Diego Sorrilha <diego@sorrilha.net>
2019-07-22 12:53:44 -0300

    Adiciona template para home
    hold #13

--- diagnostico/core/tests/test_views.py
+++ diagnostico/core/tests/test_views.py
@@ -1,6 +1,13 @@
 from django.test import Client
 
+from ..django_assertions import assert_template_used
 
-def test_status_code(client: Client):
+
+def test_home_status_code(client: Client):
     resp = client.get('/')
     assert resp.status_code == 200
+
+
+def test_home_template_used(client: Client):
+    resp = client.get('/')
+    assert_template_used(resp, 'home.html')
